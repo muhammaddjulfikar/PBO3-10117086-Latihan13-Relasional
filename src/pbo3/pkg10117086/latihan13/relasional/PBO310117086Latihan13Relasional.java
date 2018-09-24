fab0ec4e62d6f9d71d06e6fc72f0f812809ba252
djulfikar <muhammaddjulfikar7@gmail.com>
2018-09-23 22:36:43 -0700

    Relasional

---
+++ src/pbo3/pkg10117086/latihan13/relasional/PBO310117086Latihan13Relasional.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan13.relasional;
+
+/**
+ *
+ * @author Lenovo
+ *  Nama     : RD Muhammad Djulfikar BU
+ *  NIM      : 10117086
+ *  Kelas    : IF3
+ *  Minggu 23 september 2018
+ */
+public class PBO310117086Latihan13Relasional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+        
+    }
+    
+}
